6c0b6ee5cb8fac026645ba0b648ae65a063c7338
kontol98 <73051057+kontol98@users.noreply.github.com>
2020-10-20 07:10:19 +0700

    Update text.py

--- userbot/modules/text.py
+++ userbot/modules/text.py
@@ -9,33 +9,34 @@ async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("`Reply to any user message.`")
+        await event.edit("Reply to any user message.")
         return
     reply_message = await event.get_reply_message()
     # if not reply_message.text:
-    # await event.edit("```reply to text message```")
+    # await event.edit("
+reply to text message
+")
     # return
     chat = "@SmartFileUtilsBot"
     reply_message.sender
     if reply_message.sender.bot:
-        await event.edit("`Reply to actual users message.`")
+        await event.edit("Reply to actual users message.")
         return
-    await event.edit("`Scanning`")
+    await event.edit("Scanning")
     async with bot.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=1171565679 ))
+            r1 = await conv.get_response()
+            r2 = await conv.get_response()
             await bot.forward_messages(chat, reply_message)
-            response = await response
         except YouBlockedUserError:
-            await event.reply("`Please unblock   and try again`")
+            await event.reply("Please unblock   and try again")
             return
-        if response.text.startswith("Forward"):
-            await event.edit("`can you kindly disable your forward privacy settings for good?`")
+        if r1.text.startswith("Forward"):
+            await event.edit("can you kindly disable your forward privacy settings for good?")
         else:
-            await event.edit(f"```{response.message.message}```")
+            await event.edit(f"
+{r2.message.message}
+")
 
 
 CMD_HELP.update({
