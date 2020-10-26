818129a0e5cde7c94cfbe7cb78df8d8657280581
kontol98 <73051057+kontol98@users.noreply.github.com>
2020-10-26 11:28:59 +0700

    Update nulis.py

--- userbot/modules/nulis.py
+++ userbot/modules/nulis.py
@@ -6,24 +6,24 @@ from userbot import bot
 from userbot.events import register
  
 @register(outgoing=True, pattern="^\.nulis(?: |$)(.*)")
-async def _(event):
+async def _(nulis):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1)
+    link = nulis.pattern_match.group(1)
     chat = "@Rajinnulis12_bot"
     magernulis1 = f"magernulis1"
-    await event.edit("
+    await nulis.edit("
 Processing
 ")
     async with bot.conversation("@Rajinnulis12_bot") as conv:
           try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1347432560))
+              response = conv.wait_nulis(events.NewMessage(incoming=True,from_users=1347432560))
               await conv.send_message(f'/{magernulis1} {link}')
               response = await response
           except YouBlockedUserError:
-              await event.reply("
+              await nulis.reply("
 Unblock @scriptkiddies_bot plox
 ")
               return
           else:
-             await event.edit(f"{response.message.message}")
+             await nulis.edit(f"{response.message.message}")
