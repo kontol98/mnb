d316751079c61150fed0d04c75d9014222bd6fde
kontol98 <73051057+kontol98@users.noreply.github.com>
2020-10-26 11:23:59 +0700

    Update nulis.py

--- userbot/modules/nulis.py
+++ userbot/modules/nulis.py
@@ -1,24 +1,29 @@
+import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, TEMP_DOWNLOAD_DIRECTORY
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot import bot
 from userbot.events import register
-
-@register(outgoing=True, pattern=r"^\.wall(?: |$)(.*)")
+ 
+@register(outgoing=True, pattern="^\.nulis(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     link = event.pattern_match.group(1)
-    await event.edit("Processing")
+    chat = "@Rajinnulis12_bot"
+    magernulis1 = f"magernulis1"
+    await event.edit("
+Processing
+")
     async with bot.conversation("@Rajinnulis12_bot") as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=1347432560))
-            await conv.send_message(f"/magernulis1 {link}")
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("Unblock @SaitamaRobot plox")
-            return
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1347432560))
+              await conv.send_message(f'/{magernulis1} {link}')
+              response = await response
+          except YouBlockedUserError:
+              await event.reply("
+Unblock @scriptkiddies_bot plox
+")
+              return
           else:
              await event.edit(f"{response.message.message}")
