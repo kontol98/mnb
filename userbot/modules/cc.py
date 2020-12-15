900d3d172274ef4f74f98a08dd4529d707848ad9
kontol98 <73051057+kontol98@users.noreply.github.com>
2020-12-16 04:19:30 +0700

    Create cc.py

---
+++ userbot/modules/cc.py
@@ -0,0 +1,29 @@
+
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot import bot
+from userbot.events import register
+
+@register(outgoing=True, pattern="^\.cc(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@cc_pebot"
+    chk = f"chk"
+    await event.edit("```Processing```")
+    async with bot.conversation("@cc_pebot") as conv:
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1370126606))
+              await conv.send_message(f'!{chk} {link}')
+              response = await response
+          except YouBlockedUserError:
+              await event.reply("```Unblock @cc_pebot plox```")
+              return
+          else:
+             await event.delete()
+             await bot.forward_messages(event.chat_id, response.message)
+                
+                
