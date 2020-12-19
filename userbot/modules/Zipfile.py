10cb2fe1cd3c63e8f11d30cef73a444181e23741
kontol98 <73051057+kontol98@users.noreply.github.com>
2020-12-19 12:47:55 +0700

    Update Zipfile.py

--- userbot/modules/Zipfile.py
+++ userbot/modules/Zipfile.py
@@ -4,7 +4,7 @@ import zipfile
 from pySmartDL import SmartDL
 import time
 import os
-from userbot.util import admin_cmd, humanbytes, progress, time_formatter
+from userbot.utils import admin_cmd, humanbytes, progress, time_formatter
 
 @borg.on(admin_cmd(pattern="compress ?(.*)"))
 async def _(event):
